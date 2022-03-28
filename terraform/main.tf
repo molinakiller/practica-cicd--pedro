218f6a59ce952a77473835470db9f4298ba7962c
Pedro Molina <pedro.molina@techxonn.com>
2022-03-28 22:46:12 +0200

    add makefile

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,11 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.7.0"
+    }
+  }
+}
+
 provider "google" {
   credentials = "application_default_credentials.json"
-  project = "your-proyect"
+  project = "pedro-molina"
   region  = "us-central1"
   zone    = "us-central1-c"
 }
-resource "google_storage_bucket" "dev" {
-  name          = "acme-storage-dev"
+
+resource "google_storage_bucket" "acme-storage-development-keepcoding" {
+  name          = "acme-storage-dev-12434235"
   location      = "EU"
   force_destroy = true
   uniform_bucket_level_access = true
