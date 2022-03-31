9a782d140f2c4241cb3f1f3e312806700d33f1d3
molinakiller <32618431+molinakiller@users.noreply.github.com>
2022-03-31 20:35:56 +0200

    Update main.tf

--- terraform/GITHUBACTIONS/main.tf
+++ terraform/GITHUBACTIONS/main.tf
@@ -1,4 +1,22 @@
 terraform {
+  backend "remote" {
+#         # The name of your Terraform Cloud organization.
+    organization = "pedro-molina"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+#         workspaces {
+#           name = "example-workspace"
+#         }
+       }
+   }
+#
+#     # An example resource that does nothing.
+#     resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
+
   required_providers {
     google = {
       source  = "hashicorp/google"
