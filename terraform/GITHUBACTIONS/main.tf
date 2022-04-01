01d850cfea97978dd2020ccab311f3ede0de0199
molinakiller <32618431+molinakiller@users.noreply.github.com>
2022-04-01 11:15:55 +0200

    Update main.tf

--- terraform/GITHUBACTIONS/main.tf
+++ terraform/GITHUBACTIONS/main.tf
@@ -1,9 +1,18 @@
-terraform {
-  cloud {
-    organization = "acme-pedro-molina"
-
-    workspaces {
-      name = "acme-pedro-molina"
-    }
-  }
-}
+ terraform {
+       backend "remote" {
+#        # The name of your Terraform Cloud organization.
+         organization = "acme-pedro-molina"
+#
+#         # The name of the Terraform Cloud workspace to store Terraform state files in.
+        workspaces {
+           name = "acme-pedro-molina"
+         }
+       }
+     }
+#
+#     # An example resource that does nothing.
+#     resource "null_resource" "example" {
+#       triggers = {
+#         value = "A example resource that does nothing!"
+#       }
+#     }
