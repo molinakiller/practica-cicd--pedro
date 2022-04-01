edb7020683cc626defaae0407805b15cf6f44503
molinakiller <32618431+molinakiller@users.noreply.github.com>
2022-04-01 10:38:06 +0200

    Update main.tf

--- terraform/GITHUBACTIONS/main.tf
+++ terraform/GITHUBACTIONS/main.tf
@@ -9,13 +9,6 @@ terraform {
     }
   }
 }
-required_providers {
-      google = {
-        source  = "hashicorp/google"
-
-      }
-    }
-
 resource "google_storage_bucket" "acme-storage-development-keepcoding" {
   name          = "acme-storage-dev-pedro-molina"
   location      = "EU"
