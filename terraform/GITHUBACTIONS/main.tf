6c2c7d3339e9e2799e43e74f6f0335c67d14e2e3
molinakiller <32618431+molinakiller@users.noreply.github.com>
2022-04-01 10:12:16 +0200

    Update main.tf

--- terraform/GITHUBACTIONS/main.tf
+++ terraform/GITHUBACTIONS/main.tf
@@ -1,22 +1,16 @@
 terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
+  cloud {
+    organization = "acme-pedro-molina"
 
+    workspaces {
+      name = "acme-pedro-molina"
     }
   }
-}
-
-provider "google" {
-  credentials = "GCLOUD_CREDENTIALS"
-  project = "pedro-molina"
-  region  = "us-central1"
-  zone    = "us-central1-c"
-}
-
-resource "google_storage_bucket" "acme-storage-development-keepcoding" {
+  
+  resource "google_storage_bucket" "acme-storage-development-keepcoding" {
   name          = "acme-storage-dev-pedro-molina"
   location      = "EU"
   force_destroy = true
   uniform_bucket_level_access = true
+    }
 }
